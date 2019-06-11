769de3084c3a57abd4e7df8500b734fa827705d4
Maik Riechert <maik.riechert@arcor.de>
2019-06-11 08:37:27 +0100

    add Clang configs (#13)

---
+++ cmake/arch/default/Clang_C.cmake
@@ -0,0 +1,6 @@
+# WPS-CMake (https://github.com/WRF-CMake/WPS).
+# Copyright 2019 M. Riechert and D. Meyer. Licensed under the MIT License.
+
+set(debug "-g")
+set(optimized "-O3")
+set(temps "-save-temps")
